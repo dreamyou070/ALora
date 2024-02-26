e66e30aac3ac37ad2230256b40f43260cd2bbbc1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-27 07:46:50 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
 
-port_number=53113
-obj_name='transistor'
-caption='transistor'
+port_number=50001
+obj_name='screw'
+caption='screw'
 bench_mark="MVTec"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="rotating_2"
+file_name="test_2"
 
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}/models" \
+ --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
