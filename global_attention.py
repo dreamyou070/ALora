eed1853ef03edc308e33454d47127f5f80ee41eb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-27 19:11:35 +0900

    Update global_attention.py

--- global_attention.py
+++ global_attention.py
@@ -4,4 +4,4 @@ import torch
 image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k",
                                                      cache_dir="/home/dreamyou070/pretrained_model/vit-base-patch16-224-in21k")
 model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k",
-                                 cache_dir="/home/dreamyou070/pretrained_model/vit-base-patch16-224-in21k")",
+                                 cache_dir="/home/dreamyou070/pretrained_model/vit-base-patch16-224-in21k")
