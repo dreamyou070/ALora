750597d4ae84854ebaa886b6fa347c2c5324b05e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-27 21:51:26 +0900

    start of swin model

---
+++ call_swintransformer.py
@@ -0,0 +1,12 @@
+from model.swin_transformer import SwinTransformer
+import os
+import torch
+
+# [1] model config
+swin_base = '/home/dreamyou070/pretrained_model/swin_large_patch4_window12_384'
+swin_config_dir = os.path.join(swin_base, 'swin_large_patch4_window12_384_22kto1k_finetune.yaml')
+swin_model = SwinTransformer.from_config(swin_config_dir)
+
+# [2] load pretrained model
+swin_state_dict_dir = os.path.join(swin_base, 'swin_large_patch4_window12_384_22k.pth')
+swin_model.load_state_dict(torch.load(swin_state_dict_dir))
