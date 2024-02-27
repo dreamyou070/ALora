57945eff63d6cb90fe32201ee40aff626872a4fd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-28 08:39:01 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -6,7 +6,7 @@ obj_name='screw'
 caption='screw'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="test_2"
+file_name="test_global"
 
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
@@ -16,6 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
- --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --latent_res 64 --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                                    'mid_block_attentions_0_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
  --threds [0.5]
