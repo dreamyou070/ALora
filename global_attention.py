f89dfa821ae5285e8c34fb3355b71e0ac25724c6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-27 19:03:24 +0900

    Create global_attention.py

---
+++ global_attention.py
@@ -0,0 +1,7 @@
+from transformers import AutoImageProcessor, ViTModel
+import torch
+
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k",
+                                                     cache_dir="/home/dreamyou070/pretrained_model/vit-base-patch16-224-in21k")
+model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k",
+                                 cache_dir="/home/dreamyou070/pretrained_model/vit-base-patch16-224-in21k")",
