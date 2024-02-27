04ed3b77719840e3a0478a7c25b07ea736eff761
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-27 19:25:13 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -32,5 +32,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_normal_sample --do_object_detection --do_rotate_anomal_sample \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_attn_loss --do_map_loss \
- --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',]" \
+ --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                    'mid_block_attentions_0_transformer_blocks_0_attn2']" \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1
