23a02eecdd5ca8829fd2faf48ea2af8ff49cb067
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-28 11:46:20 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
-port_number=50003
+port_number=50005
 bench_mark="MVTec"
 obj_name='transistor'
 trigger_word='transistor'
-layer_name='layer_3'
-sub_folder="up_16_32_64"
+layer_name='layer_4'
+sub_folder="mid_up_16_32_64"
 file_name="test_global_3_back_normal"
 
 anomal_source_path="../../../MyData/anomal_source"
@@ -29,8 +29,11 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --back_max_beta_scale 0.9 \
  --back_trg_beta 0 \
  --do_rot_augment \
- --do_background_masked_sample --do_object_detection --do_normal_sample \
+ --do_background_masked_sample --do_object_detection --do_anormal_sample \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_attn_loss --do_map_loss \
- --trg_layer_list "['mid_block_attentions_0_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['mid_block_attentions_0_transformer_blocks_0_attn2',
+                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1
