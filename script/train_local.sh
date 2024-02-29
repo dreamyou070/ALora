6499a6368a9ebe7963440feb9cade916d66f3cff
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-29 11:17:09 +0900

    Update train_local.sh

--- script/train_local.sh
+++ script/train_local.sh
@@ -4,20 +4,20 @@ port_number=50009
 bench_mark="MVTec"
 obj_name='transistor'
 trigger_word='transistor'
-layer_name='layer_3'
-sub_folder="up_16_32_64"
-file_name="test_train_local_scaled_query_only_back_anomal"
+layer_name='layer_4'
+sub_folder="mid_up_16_32_64"
+file_name="test_train_local_scaled_query_big_backsample"
 
 anomal_source_path="../../../MyData/anomal_source"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_local.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
  --trigger_word "${trigger_word}" \
- --obj_name "${obj_name}" --anomal_only_on_object \
+ --obj_name "${obj_name}" --do_object_detection --anomal_only_on_object \
  --anomal_source_path "${anomal_source_path}" \
  --anomal_min_perlin_scale 0 \
  --anomal_max_perlin_scale 6 \
@@ -26,10 +26,11 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --back_min_perlin_scale 0 \
  --back_max_perlin_scale 2 \
  --back_trg_beta 0 \
- --do_normal_sample --do_background_masked_sample --do_object_detection \
+ --do_anomal_sample --do_background_masked_sample --do_object_detection \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_map_loss \
- --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+ --trg_layer_list "['mid_block_attentions_0_transformer_blocks_0_attn2',
+                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1
