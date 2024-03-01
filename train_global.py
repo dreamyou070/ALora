4420fd2d742bb8407373f6dbac358693b1729cdc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 12:59:29 +0900

    train global

--- train_global.py
+++ train_global.py
@@ -491,6 +491,10 @@ if __name__ == "__main__":
     parser.add_argument("--anomal_trg_beta", type=float)
     parser.add_argument("--back_trg_beta", type=float)
     parser.add_argument("--on_desktop", action='store_true')
+    parser.add_argument("--all_positional_embedder", action='store_true')
+    parser.add_argument("--all_self_cross_positional_embedder", action='store_true')
+    parser.add_argument("--patch_positional_self_embedder", action='store_true')
+    parser.add_argument("--use_multi_position_embedder", action='store_true')
     # -----------------------------------------------------------------------------------------------------------------
     args = parser.parse_args()
     unet_passing_argument(args)
