4379e77e7649a689f8b9f7bf6ae61f72f45caf93
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 23:44:54 +0900

    a

--- make_pe_net.py
+++ make_pe_net.py
@@ -45,8 +45,33 @@ def main(args):
     weight_dtype, save_dtype = prepare_dtype(args)
     text_encoder, vae, unet, network, position_embedder = call_model_package(args, weight_dtype, accelerator, True)
 
+    layer_names_res_dim = {'down_blocks_0_attentions_0_transformer_blocks_0_attn2': (64, 320),
+                           'down_blocks_0_attentions_1_transformer_blocks_0_attn2': (64, 320),
+
+                           'down_blocks_1_attentions_0_transformer_blocks_0_attn2': (32, 640),
+                           'down_blocks_1_attentions_1_transformer_blocks_0_attn2': (32, 640),
+
+                           'down_blocks_2_attentions_0_transformer_blocks_0_attn2': (16, 1280),
+                           'down_blocks_2_attentions_1_transformer_blocks_0_attn2': (16, 1280),
+
+                           'mid_block_attentions_0_transformer_blocks_0_attn2': (8, 1280),
+
+                           'up_blocks_1_attentions_0_transformer_blocks_0_attn2': (16, 1280),
+                           'up_blocks_1_attentions_1_transformer_blocks_0_attn2': (16, 1280),
+                           'up_blocks_1_attentions_2_transformer_blocks_0_attn2': (16, 1280),
+
+                           'up_blocks_2_attentions_0_transformer_blocks_0_attn2': (32, 640),
+                           'up_blocks_2_attentions_1_transformer_blocks_0_attn2': (32, 640),
+                           'up_blocks_2_attentions_2_transformer_blocks_0_attn2': (32, 640),
+
+                           'up_blocks_3_attentions_0_transformer_blocks_0_attn2': (64, 320),
+                           'up_blocks_3_attentions_1_transformer_blocks_0_attn2': (64, 320),
+                           'up_blocks_3_attentions_2_transformer_blocks_0_attn2': (64, 320), }
+
+
     for layer_name, net in unet.named_children():
-        if net.__class__.__name__ == 'CrossAttention':
+        if net.__class__.__name__ == 'CrossAttention' and layer_name in layer_names_res_dim.keys() :
+
             print(f'layer_name = {layer_name}')
 
 
