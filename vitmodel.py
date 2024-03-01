1c62014ab2f595633da70e974f71502349a6ee63
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 09:47:33 +0900

    vitmodel.py

--- vitmodel.py
+++ vitmodel.py
@@ -1,5 +1,11 @@
 from transformers import ViTImageProcessor, ViTForImageClassification
+from PIL import Image
 
 vit_dir = r'/home/dreamyou070/.cache/huggingface/hub/models--google--vit-base-patch16-224/snapshots/3f49326eb077187dfe1c2a2bb15fbd74e6ab91e3'
 processor = ViTImageProcessor.from_pretrained(vit_dir)
 model = ViTForImageClassification.from_pretrained(vit_dir)
+
+img_dir = r'/home/dreamyou070/MyData/anomal_source/dtd_images/banded/banded_0002.jpg'
+image = Image.open(img_dir).convert('RGB')
+inputs = processor(images=image, return_tensors="pt")
+print(f'inputs : {inputs.shape}')
