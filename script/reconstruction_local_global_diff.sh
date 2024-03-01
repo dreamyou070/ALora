d2c41a55d4a1161d03c5b9075255cc1ba66a4a15
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 17:03:59 +0900

    recon

--- script/reconstruction_local_global_diff.sh
+++ script/reconstruction_local_global_diff.sh
@@ -12,7 +12,7 @@ file_name="train_global_masking"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../reconstruction_vae.py \
+ --main_process_port $port_number ../reconstruction_local_global_diff.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
