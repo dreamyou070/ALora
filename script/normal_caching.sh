e35cbe102f125bcc0ce5e082b9b714f9799cd238
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-02 07:08:25 +0900

    object again

--- script/normal_caching.sh
+++ script/normal_caching.sh
@@ -20,4 +20,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --do_object_detection \
+ --obj_name "${obj_name}" \
  --trigger_word "${obj_name}" --all_positional_embedder
