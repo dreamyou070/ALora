fd1913f568908e8ce6dd9a1395277a05717411a3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 17:09:44 +0900

    --all_positional_embedder

--- script/reconstruction_local_global_diff.sh
+++ script/reconstruction_local_global_diff.sh
@@ -22,5 +22,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
+ --d_dim 320 --use_position_embedder --all_positional_embedder ${position_embedding_layer} \
  --threds [0.5]
