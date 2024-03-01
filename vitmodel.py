372f67ac323ac58981a7a5cbb5cca61a394b605a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 09:44:51 +0900

    vit dir

--- vitmodel.py
+++ vitmodel.py
@@ -1,4 +1,5 @@
 from transformers import ViTImageProcessor, ViTForImageClassification
 
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+vit_dir = r'/home/dreamyou070/.cache/huggingface/hub/models--google--vit-base-patch16-224/snapshots/3f49326eb077187dfe1c2a2bb15fbd74e6ab91e3'
+processor = ViTImageProcessor.from_pretrained(vit_dir)
+model = ViTForImageClassification.from_pretrained(vit_dir)
