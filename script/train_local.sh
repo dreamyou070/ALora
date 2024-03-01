f1b2588ce3d1cdf4721bcd3fb6e5ea6291cd36b9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-01 19:19:08 +0900

    train local

--- script/train_local.sh
+++ script/train_local.sh
@@ -10,7 +10,7 @@ file_name="local_no_pe"
 
 anomal_source_path="../../../MyData/anomal_source"
 # --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
  --main_process_port $port_number ../train_local.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
