abef4808491b01b6f7efcf20f158d6cdb86aa643
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-02 00:33:45 +0900

    Update train_global.sh

--- script/train_global.sh
+++ script/train_global.sh
@@ -2,18 +2,18 @@
 # scratch vae / trained vae
 # matching loss : only normal
 
-port_number=50000
+port_number=50001
 bench_mark="MVTec"
 obj_name='transistor'
 trigger_word='transistor'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="from_trained_pe_local_global_from_scratch_vae"
+file_name="from_trained_pe_local_global_from_pretrained_vae"
 
 anomal_source_path="../../../MyData/anomal_source"
 network_weights="../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/local_all_crossattn_pe/models/epoch-000009.safetensors" \
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_global_masking.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
@@ -34,7 +34,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --back_trg_beta 0 \
  --do_anomal_sample --do_object_detection --do_background_masked_sample --global_net_normal_training \
  --local_use_position_embedder \
- --train_vae \
  --trg_layer_list "['mid_block_attentions_0_transformer_blocks_0_attn2',
                     'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
