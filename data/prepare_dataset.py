b84d57c87a8898d53da24ae46d7ab8466f6d0001
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-02 16:36:50 +0900

    blurring test

--- data/prepare_dataset.py
+++ data/prepare_dataset.py
@@ -29,7 +29,7 @@ def call_dataset(args) :
                          anomal_training=True,
                          latent_res=args.latent_res,
                          do_anomal_sample =args.do_anomal_sample,
-                         use_object_mask = args.do_object_detection)
+                         use_object_mask = args.do_object_detection,)
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=args.batch_size,
                                              shuffle=True)
--- script/train_local_blur.sh
+++ script/train_local_blur.sh
@@ -27,7 +27,9 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_anomal_sample \
  --use_position_embedder \
  --do_map_loss \
+ --do_object_detection \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
- --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1
+ --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
+ --blurring_test
