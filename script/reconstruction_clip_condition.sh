9186830b5554aff86d45b632a48c67c5b718d6bb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-03 23:17:40 +0900

    transistor train

--- script/reconstruction_clip_condition.sh
+++ script/reconstruction_clip_condition.sh
@@ -8,6 +8,6 @@ sub_folder="train_unet_background_sample"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_clip_condition.py \
- --output_dir "../../result/${obj_name}/unet_train/${sub_folder}/" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/unet_train/${sub_folder}/" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}"
