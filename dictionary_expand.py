5fb5e97245c58d59f7d2903f853dc881572fd78b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-04 14:47:21 +0900

    train answer test

--- dictionary_expand.py
+++
@@ -1,5 +0,0 @@
-import os
-
-dic1 = {'a' : 3}
-dic2 = {'b' : 4}
-
--- global_attention.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoImageProcessor, ViTModel
-import torch
-
-image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k",)
-model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k",)
-model.save_pretrained(r"/home/dreamyou070/pretrained_model")
-image_processor.save_pretrained(r"/home/dreamyou070/pretrained_model")
