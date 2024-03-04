d57d79c1696f2ebbf89ac89c3f63a52a089deb38
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-04 20:45:33 +0900

    train self attn test

--- script/train_answer_test_self.sh
+++ script/train_answer_test_self.sh
@@ -10,11 +10,13 @@ file_name="test_20240303_down_selfattn_gt"
 
 anomal_source_path="../../../MyData/anomal_source"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_answer_test_self.py --log_with wandb \
  --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+ --network_weights "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000009.safetensors" \
+ --position_embedder_weights "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder/position_embedder_9.safetensors" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" --do_object_detection --anomal_only_on_object \
