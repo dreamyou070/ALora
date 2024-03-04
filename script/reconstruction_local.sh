5e10d143d6d7fd21ddd14821ab8981a80ec4fdb5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-04 09:31:35 +0900

    recon

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -5,8 +5,8 @@ bench_mark="MVTec"
 obj_name='transistor'
 caption='transistor'
 layer_name='layer_3'
-sub_folder="up_16_32_64"
-file_name="test_20240303_with_gt_answer_check"
+sub_folder="down_16_32_64"
+file_name="test_20240303_with_gcn"
 
 # position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 #--d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
@@ -24,4 +24,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
                     'up_blocks_3_attentions_1_transformer_blocks_0_attn2']" \
  --threds [0.5] \
  --use_position_embedder \
- --all_positional_embedder
+ --all_positional_embedder --use_global_conv
