fccea73c11f3e46339bf700160dec9cc2c0948c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-04 20:20:03 +0900

    recon local

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -4,9 +4,9 @@ port_number=50333
 bench_mark="MVTec"
 obj_name='transistor'
 caption='transistor'
-layer_name='layer_6'
-sub_folder="up_down_16_32_64"
-file_name="test_20240303_with_gt_answer_check"
+layer_name='layer_3'
+sub_folder="down_16_32_64"
+file_name="test_20240304_with_backsample"
 
 # position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 #--d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
@@ -21,10 +21,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --latent_res 64 \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2',
                     'down_blocks_1_attentions_1_transformer_blocks_0_attn2',
-                    'down_blocks_2_attentions_1_transformer_blocks_0_attn2',
-                    'up_blocks_1_attentions_2_transformer_blocks_0_attn2',
-                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
-                    'up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+                    'down_blocks_2_attentions_1_transformer_blocks_0_attn2',]" \
  --threds [0.5] \
  --use_position_embedder \
  --all_positional_embedder
