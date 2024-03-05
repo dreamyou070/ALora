b76c75736d1a85b43e1a5d7f6eb069f3623347d9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 02:40:04 +0900

    recon local

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -6,10 +6,10 @@ obj_name='teeth_crop'
 caption='teeth'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="test_20240305_with_gt_answer_check_with_pe_cropped_image"
+file_name="test_20240305_with_gt_anomal_answer_check_with_pe_cropped_image"
 
 # position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
-#--d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
+# --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
 # --use_position_embedder --all_positional_embedder \
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
@@ -20,8 +20,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --do_train_check \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 \
- --trg_layer_list "['up_blocks_1_attentions_1_transformer_blocks_0_attn2',
-                    'up_blocks_2_attentions_1_transformer_blocks_0_attn2',
-                    'up_blocks_3_attentions_1_transformer_blocks_0_attn2',]" \
+ --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --threds [0.5] \
  --do_train_check --use_position_embedder --all_positional_embedder
