3249f471cd4894e336b87b20824301a1d2ff2e07
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-05 20:17:42 +0900

    Update reconstruction_local.sh

--- script/reconstruction_local.sh
+++ script/reconstruction_local.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
 port_number=50333
-bench_mark="MVTec"
+bench_mark="Tuft"
 obj_name='transistor'
 caption='transistor'
 layer_name='layer_3'
-sub_folder="down_16_32_64"
-file_name="test_20240303_down_not_anomal_only_on_object"
+sub_folder="up_16_32_64"
+file_name="test_20240305_with_gt_answer_check"
 
 # position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 #--d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} \
@@ -15,13 +15,13 @@ file_name="test_20240303_down_not_anomal_only_on_object"
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_local.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
+ --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${layer_name}/${sub_folder}/${file_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 \
- --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2',
-                    'down_blocks_1_attentions_1_transformer_blocks_0_attn2',
-                    'down_blocks_2_attentions_1_transformer_blocks_0_attn2',]" \
+ --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+                    'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --threds [0.5] \
  --use_position_embedder \
  --all_positional_embedder
